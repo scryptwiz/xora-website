e6ec8b737f5d7c2086405b9befa4a1fae9db10d8
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2024-12-09 09:45:15 +0100

    feat(Hero): Completed the hero section

--- src/assets/images/index.jsx
+++ src/assets/images/index.jsx
@@ -1,8 +1,10 @@
 // Icons
 export {default as MagicIcon} from '/images/magic.svg'
 export {default as CloseIcon} from '/images/close.svg'
+export {default as ZapIcon} from '/images/zap.svg'
 
 // Images
 export {default as HeroLogo} from '/images/xora.svg'
 export {default as Outline} from '/images/bg-outlines.svg'
 export {default as OutlineFill} from '/images/bg-outlines-fill.png'
+export {default as HeroImage} from '/images/hero.png'
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,6 +1,27 @@
+import {Element, Link as LinkScroll} from "react-scroll"
+import {Button} from "../components"
+import {HeroImage, ZapIcon} from "../assets/images"
+
 const Hero = () => {
 	return (
-		<section className="relative border-2 border-amber-400 pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-mb:pb-32"></section>
+		<section className="relative pt-60 pb-40 max-lg:pt-52 max-lg:pb-36 max-md:pt-36 max-mb:pb-32">
+			<Element name="hero">
+				<div className="container">
+					<div className="relative z-2 max-w-512 max-lg:w-388">
+						<div className="caption small-2 uppercase text-p3">Video Editing</div>
+						<h1 className="mb-6 h1 text-p4 upercase max-lg:mb-7 max-lg:h2 max-md:mb-4 max-md:text-5xl max-md:leading-12">Amazingly simple</h1>
+						<p className="max-w-440 mb-14 body-1 max-md:mb-10">We designed XORA AI Vedio Editor to be an easy to use, quick to learn, and surprisingly powerful.</p>
+						<LinkScroll to="features" offset={-100} spy smooth>
+							<Button icon={ZapIcon}>Try it now</Button>
+						</LinkScroll>
+					</div>
+
+					<div className="absolute -top-32 left-[calc(50%-340px)] w-[1230px] pointer-events-none hero-img_res">
+						<img src={HeroImage} className="size-1230 max-lg:h-auto" alt="hero" />
+					</div>
+				</div>
+			</Element>
+		</section>
 	)
 }
 
