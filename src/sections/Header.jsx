2c6ab39e1de5487e7d1143f60e7895976527772d
Kelvin Ajayi <68742854+scryptwiz@users.noreply.github.com>
2024-12-12 21:10:06 +0100

    feat(features): Added fetures secion

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,61 +1,84 @@
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import {CloseIcon, HeroLogo, MagicIcon, Outline, OutlineFill} from "../assets/images"
 import {Link as LinkScroll} from "react-scroll"
 import clsx from "clsx"
 
-const NavLink = ({title}) => (
-	<LinkScroll className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
-		{title}
-	</LinkScroll>
-)
-
 const Header = () => {
-	const [isOpen, setIsOpen] = useState(false)
+    const [hasScroll, setHasScroll] = useState(false)
+    const [isOpen, setIsOpen] = useState(false)
 
-	return (
-		<header className="fixed top-0 left-0 z-50 w-full py-10">
-			<div className="container flex h-14 items-center max-lg:px-5">
-				<a className="lg:hidden flex-1 cursor-pointer z-2">
-					<img src={HeroLogo} width={115} height={55} alt="logo" />
-				</a>
+    useEffect(() => {
+        const handleScroll = () => {
+            setHasScroll(window.scrollY > 32)
+        }
 
-				<div className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2', isOpen ? 'max-lg:opacity-100' : 'max-lg:opacity-0')}>
-					<div className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
-						<nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
-							<ul className="flex max-lg:block max-lg:px-12">
-								<li className="nav-li">
-									<NavLink title='features' />
-									<div className="dot" />
-									<NavLink title='pricing' />
-								</li>
+        window.addEventListener('scroll', handleScroll);
 
-								<li className="nav-logo">
-									<LinkScroll to="hero" offset={-100} spy smooth className={clsx('max-lg:hidden transform duration-500 cursor-pointer')}>
-										<img src={HeroLogo} width={165} height={55} alt="logo" />
-									</LinkScroll>
-								</li>
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, []);
 
-								<li className="nav-li">
-									<NavLink title='faq' />
-									<div className="dot" />
-									<NavLink title='download' />
-								</li>
-							</ul>
-						</nav>
+    const NavLink = ({title}) => (
+        <LinkScroll onClick={() => setIsOpen(false)} to={title} spy smooth offset={-100} activeClass="nav-active"
+                    className="base-bold text-p4 uppercase transition-colors duration-500 cursor-pointer hover:text-p1 max-lg:my-4 max-lg:h5">
+            {title}
+        </LinkScroll>
+    )
 
-						<div className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
-							<img src={Outline} width={960} height={380} alt="outline" className="relative z-2" />
-							<img src={OutlineFill} width={960} height={380} alt="outline" className="absolute insert-0 mix-blend-soft-light opacity-5" />
-						</div>
-					</div>
-				</div>
+    return (
+        <header
+            className={clsx('fixed top-0 left-0 z-50 w-full py-10', hasScroll && 'py-2 bg-black-100 backdrop-blur-[8px] max-lg:py-4 transition-all duration-500')}>
+            <div className="container flex h-14 items-center max-lg:px-5">
+                <a className="lg:hidden flex-1 cursor-pointer z-2">
+                    <img src={HeroLogo} width={115} height={55} alt="logo"/>
+                </a>
 
-				<button className="lg:hidden z-2 size-10 border-2 border-s4/25 flex rounded-full justify-center items-center" onClick={() => setIsOpen((prevState) => !prevState)}>
-					<img src={isOpen ? CloseIcon : MagicIcon} alt={isOpen ? "close_icon" : "magic_icon"} className="size-1/2 object-contain" />
-				</button>
-			</div>
-		</header>
-	)
+                <div
+                    className={clsx('w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2', isOpen ? 'max-lg:opacity-100' : 'max-lg:opacity-0')}>
+                    <div
+                        className="max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg:p-6 max-lg:overflow-hidden sidebar-before max-md:px-4">
+                        <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
+                            <ul className="flex max-lg:block max-lg:px-12">
+                                <li className="nav-li">
+                                    <NavLink title='features'/>
+                                    <div className="dot"/>
+                                    <NavLink title='pricing'/>
+                                </li>
+
+                                <li className="nav-logo">
+                                    <LinkScroll to="hero" offset={-250} spy smooth
+                                                className={clsx('max-lg:hidden transform duration-500 cursor-pointer')}>
+                                        <img src={HeroLogo} width={165} height={55} alt="logo"/>
+                                    </LinkScroll>
+                                </li>
+
+                                <li className="nav-li">
+                                    <NavLink title='faq'/>
+                                    <div className="dot"/>
+                                    <NavLink title='download'/>
+                                </li>
+                            </ul>
+                        </nav>
+
+                        <div
+                            className="lg:hidden block absolute top-1/2 left-0 w-[960px] h-[380px] translate-x-[-290px] -translate-y-1/2 rotate-90">
+                            <img src={Outline} width={960} height={380} alt="outline" className="relative z-2"/>
+                            <img src={OutlineFill} width={960} height={380} alt="outline"
+                                 className="absolute insert-0 mix-blend-soft-light opacity-5"/>
+                        </div>
+                    </div>
+                </div>
+
+                <button
+                    className="lg:hidden z-2 size-10 border-2 border-s4/25 flex rounded-full justify-center items-center"
+                    onClick={() => setIsOpen((prevState) => !prevState)}>
+                    <img src={isOpen ? CloseIcon : MagicIcon} alt={isOpen ? "close_icon" : "magic_icon"}
+                         className="size-1/2 object-contain"/>
+                </button>
+            </div>
+        </header>
+    )
 }
 
 export default Header
